be607d8d45c7f74247965287d01572c21237a86e
Thomas Waters <twaters117@gmail.com>
2021-03-12 10:08:58 -0500

    Comparing Agent Behaviors

--- agent.py
+++ agent.py
@@ -1,4 +1,6 @@
-from game import Game
+import random
+
+from game import Game, Match
 
 
 class Agent:
@@ -14,5 +16,36 @@ class Agent:
             game.clear_cards()
             return
 
+        random.shuffle(moves)
         moves.sort(key=lambda match: match.value)
+        game.play(moves[0])
+
+    @staticmethod
+    def play_run_first(game: Game) -> None:
+        """
+        Modifies the current game state to take an action
+        :param game: The current game state
+        """
+        moves = game.moves()
+
+        if len(moves) == 0:
+            game.clear_cards()
+            return
+
+        moves.sort(key=lambda match: match.value + (match.type == Match.Type.Pair))
+        game.play(moves[0])
+
+    @staticmethod
+    def play_pair_first(game: Game) -> None:
+        """
+        Modifies the current game state to take an action
+        :param game: The current game state
+        """
+        moves = game.moves()
+
+        if len(moves) == 0:
+            game.clear_cards()
+            return
+
+        moves.sort(key=lambda match: match.value + (match.type == Match.Type.Pair))
         game.play(moves[0])
--- main.py
+++ main.py
@@ -3,9 +3,51 @@ import matplotlib.pyplot as plt
 from game import Game
 from agent import Agent
 from cooperative import Cooperative
+from statistics import mean
 
 
 def main():
+    agent_comparison()
+
+
+def agent_comparison():
+    players = 1
+    grid_size = (3, 4)
+    iterations = 100000
+    bin_width = 1
+
+    fig, axs = plt.subplots(3, 1)
+
+    random_agent_scores = [play_game(players, grid_size, Agent.play) for _ in range(iterations)]
+    run_first_scores = [play_game(players, grid_size, Agent.play_run_first) for _ in range(iterations)]
+    pair_first_scores = [play_game(players, grid_size, Agent.play_pair_first) for _ in range(iterations)]
+
+    max_range = max(pair_first_scores + run_first_scores + random_agent_scores)
+    min_range = min(pair_first_scores + run_first_scores + random_agent_scores)
+
+    bins = range(min_range, max_range + bin_width, bin_width)
+
+    axis = axs[0]
+    axis.set_title(f'Max Value Random Agent, average = {mean(random_agent_scores)}')
+    axis.hist(random_agent_scores, bins=bins, density=True)
+
+    axis = axs[1]
+    axis.set_title(f'Run First Agent, average = {mean(run_first_scores)}')
+    axis.hist(run_first_scores, bins=bins, density=True)
+
+    axis = axs[2]
+    axis.set_title(f'Pair First Agent, average = {mean(pair_first_scores)}')
+    axis.hist(pair_first_scores, bins=bins, density=True)
+
+    # Set common labels
+    fig.text(0.5, 0.04, 'Game Score', ha='center', va='center')
+    fig.text(0.06, 0.5, 'Probability Distribution', ha='center', va='center', rotation='vertical')
+
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
+
+
+def player_count_vs_grid_size():
     max_players = 4
     players = range(1, max_players + 1)
     grid_sizes = [(3, 3), (3, 4)]
@@ -15,7 +57,7 @@ def main():
 
     for (x, player_count) in enumerate(players):
         for (y, grid_size) in enumerate(grid_sizes):
-            scores = [play_game(player_count, grid_size) for _ in range(iterations)]
+            scores = [play_game(player_count, grid_size, Agent.play) for _ in range(iterations)]
 
             bin_width = 1
             bins = range(min(scores), max(scores) + bin_width, bin_width)
@@ -23,16 +65,17 @@ def main():
             rows, columns = grid_size
             axis = axs[x, y]
             axis.hist(scores, bins=bins, density=True)
-            axis.set_title(f"{player_count} Players on {rows}x{columns}")
+            axis.set_title(f'{player_count} Players on {rows}x{columns}')
 
     fig.tight_layout()
     plt.show()
 
 
-def play_game(player_count, grid_size: (int, int)) -> int:
+def play_game(player_count, grid_size: (int, int), agent) -> int:
     """
     Play a single game of scrounge and get the resulting score
 
+    :param agent: The agent that takes in the game state and takes a turn
     :param player_count: The number of players playing the game
     :param grid_size: The size of the game grid
     :return: The score from playing the game
@@ -44,7 +87,7 @@ def play_game(player_count, grid_size: (int, int)) -> int:
         game = Cooperative(rows, columns, player_count)
 
     while not game.completed():
-        Agent.play(game)
+        agent(game)
 
     return game.score
 
